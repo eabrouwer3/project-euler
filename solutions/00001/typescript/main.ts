d18243cafe57842f50c3a6e5fce50f38c8c54942
Ethan Brouwer <1647525+eabrouwer3@users.noreply.github.com>
2025-07-15 11:37:19 -0400

    Typescript solution for problem 1 actually works and isn't just hello world haha

--- solutions/00001/typescript/main.ts
+++ solutions/00001/typescript/main.ts
@@ -1,3 +1,13 @@
+function *range(a: number, b?: number, step: number = 1): Generator<number> {
+  for (let i = a; !b || i < b; i += step) {
+    yield i
+  }
+}
+
 if (import.meta.main) {
-  console.log('Hello, World!')
+  const multiplesOf3 = new Set(Array.from(range(3, 1000, 3)))
+  const multiplesOf5 = new Set(Array.from(range(5, 1000, 5)))
+  const allMultiples = multiplesOf3.union(multiplesOf5)
+  const multipleSum = Array.from(allMultiples).reduce((a, b) => a + b)
+  console.log(multipleSum)
 }
